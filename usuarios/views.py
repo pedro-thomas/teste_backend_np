28c7c9c849791c78b9adeca57fc5a91ad7c8e80b
Pedro Thomas <impedrothomas@outlook.com>
2024-02-01 14:52:34 -0300

    autenticação para usuarios

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
 from .models import Usuario
 from .serializers import UsuarioSerializer
+from rest_framework.permissions import IsAuthenticated
 
 class UsuarioViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Usuario.objects.all()
     serializer_class = UsuarioSerializer
